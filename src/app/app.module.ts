e4c66729793347153be26162d60d0b59d3d18182
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2018-07-25 21:41:55 +1200

    added emergency page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {EmergencyPage} from '../pages/emergency/emergency';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    EmergencyPage
   ],
   imports: [
     BrowserModule,
@@ -35,7 +37,8 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    EmergencyPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/emergency/emergency.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-emergency',
+  templateUrl: 'emergency.html'
+})
+export class EmergencyPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
